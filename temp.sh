394eb8682a49f8ec76576b3ae9a350af7459a12a
Rodrigo R. Marinho <marinho.rodrigo@gmail.com>
2025-04-21 11:02:04 -0300

    Update temp.sh

--- temp.sh
+++ temp.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 cd "$(dirname "$0")"
+python3 -m venv venv
 source venv/bin/activate
-python3 teste_browser.py
+pip install -r requirements.txt
+playwright install
+pytest teste_browser.py
